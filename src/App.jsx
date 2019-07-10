1dedf6db1de5c84d02fe36697d89188dee512975
Denny Paras <dennyparas2@gmail.com>
2019-07-10 15:36:46 +0800

    setup redux store and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Pages Component
+import Home from './pages/Home';
+
+// Redux Store
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
-  return <div className='App'>App</div>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
